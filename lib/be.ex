969eee541d7c0f588dfde413b673f67f3c3f1f07
Helder Sousa <helderhenri@gmail.com>
2022-06-13 13:33:34 -0300

    add uique function

--- lib/be.ex
+++ lib/be.ex
@@ -1,2 +1,12 @@
 defmodule Be do
+
+  def unique(len) do
+    gen_rnd(len, "abcdefghijklmnopqrstuvwxyz1234567890")
+  end
+  defp gen_rnd(to, al) do
+    # DateTime.utc_now |> DateTime.to_unix(:millisecond)
+    len = String.length(al)
+    x = fn _x -> String.at(al, :rand.uniform(len)) end
+    1..to |> Enum.map_join(x)
+  end
 end
