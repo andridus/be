665f3a5d4c089fe5a2fe910d91c0b6525baecba9
Helder Sousa <helderhenri@gmail.com>
2022-11-17 16:15:53 -0300

    revert

--- lib/api.ex
+++ lib/api.ex
@@ -26,7 +26,7 @@ defmodule Be.Api do
       def get_by!(params \\ [where: [], order: [asc: :inserted_at]]) do
         params
         |> default_params()
-        |> __repo__().one()
+        |> repo().one()
       end
 
       def get(_id, _params \\ [where: [], order: [asc: :inserted_at]])
@@ -43,7 +43,7 @@ defmodule Be.Api do
       def get!(id, params \\ [where: [], order: [asc: :inserted_at]]) do
         params
         |> default_params()
-        |> __repo__().get(id)
+        |> repo().get(id)
       end
 
       def blank(), do: struct(schema())
@@ -51,27 +51,27 @@ defmodule Be.Api do
       def all(params \\ [where: [], order: []]) do
         params
         |> default_params()
-        |> __repo__().all()
+        |> repo().all()
       end
 
       def insert(%Ecto.Changeset{} = model),
-        do: model |> __repo__().insert()
+        do: model |> repo().insert()
 
       def insert(params) do
         schema()
         |> struct()
         |> schema().changeset_insert(params)
-        |> __repo__().insert()
+        |> repo().insert()
       end
 
       def exists?(params) do
         params
         |> default_params()
-        |> __repo__().exists?()
+        |> repo().exists?()
       end
 
       def update(%Ecto.Changeset{} = model),
-        do: model |> __repo__().update()
+        do: model |> repo().update()
 
       def update(%{"id" => id} = model) do
         params = Map.drop(model, ["id"])
@@ -91,13 +91,13 @@ defmodule Be.Api do
         id
         |> get!()
         |> schema().changeset_update(params)
-        |> __repo__().update()
+        |> repo().update()
       end
 
       def delete(id) when is_bitstring(id),
         do: id |> get!() |> delete()
 
-      def delete(model), do: model |> __repo__().delete()
+      def delete(model), do: model |> repo().delete()
 
       def default_params(params, sc \\ nil) do
         schm_ = sc || schema()
@@ -243,7 +243,7 @@ defmodule Be.Api do
       def count(params \\ []) do
         params
         |> default_params()
-        |> __repo__().aggregate(:count, :id)
+        |> repo().aggregate(:count, :id)
       end
 
       @doc """
@@ -266,8 +266,6 @@ defmodule Be.Api do
       def insert_or_update(%{id: id} = model) when not is_nil(id), do: update(model)
       def insert_or_update(model), do: insert(model)
 
-      def __repo__, do: Be.Api.repo()
-
       defoverridable changeset: 2,
                      changeset: 3,
                      json_fields: 1,
@@ -283,7 +281,7 @@ defmodule Be.Api do
     end
   end
 
-  def repo, do: Application.get_env(:be, :repo) || raise "Need to be defined in your config.exs 'config :be, repo: YouApp.Repo'"
+  def repo, do: Application.get_env(:be, :repo)
 
   def preload_json(model, include \\ []) do
     model
@@ -317,6 +315,7 @@ defmodule Be.Api do
     ArgumentError -> String.to_atom(string)
   end
 
-  defp unwrap({:ok, value}), do: value
-  defp unwrap(err), do: err
+  def unwrap({:ok, value}), do: value
+  def unwrap(err), do: err
+  def unwrap!({_, value}), do: value
 end
